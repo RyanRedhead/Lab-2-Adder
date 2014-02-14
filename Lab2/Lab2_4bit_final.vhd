147b022dfbf7ba01dfb4e247bce936b9295994dc
RyanRedhead <C16Ryan.Redhead@usafa.edu>
2014-02-14 00:38:31 -0700

    final stuff

---
+++ Lab2/Lab2_4bit_final.vhd
@@ -0,0 +1,123 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:03:33 02/13/2014 
+-- Design Name: 
+-- Module Name:    Lab2_4bit_final - Structural 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab2_4bit_final is
+    Port ( A : in  STD_LOGIC_VECTOR (3 downto 0);
+           B : in  STD_LOGIC_VECTOR (3 downto 0);
+           SubChoice : in  STD_LOGIC;
+           Sum : inout  STD_LOGIC_VECTOR (3 downto 0);
+           Cout : out  STD_LOGIC;
+           Overflow : out  STD_LOGIC);
+end Lab2_4bit_final;
+
+architecture Structural of Lab2_4bit_final is
+
+component bitadder
+port(A: IN STD_LOGIC;
+B: IN STD_LOGIC;
+Cin: IN STD_LOGIC;
+Sum: INOUT STD_LOGIC;
+Cout: OUT STD_LOGIC
+);
+end component;
+
+component mux
+port(
+D0 : IN STD_LOGIC_VECTOR (3 downto 0);
+D1 : IN STD_LOGIC_VECTOR (3 downto 0);
+S : IN STD_LOGIC;
+Y : OUT STD_LOGIC_VECTOR (3 downto 0)
+);
+end component;
+
+component overflowdetector
+port(
+Amost : in  STD_LOGIC;
+Bmost : in  STD_LOGIC;
+Summost : in  STD_LOGIC;
+overflo : out  STD_LOGIC
+);
+end component;
+
+signal Cin1, Cin2, Cin3 : STD_LOGIC;
+signal B_NOT, Bother : STD_LOGIC_VECTOR (3 downto 0);
+
+begin
+
+B_NOT <= not B;
+
+Mux1 : mux port map(
+D0 => B,
+D1 => B_NOT,
+S =>SubChoice,
+Y => Bother
+);
+
+OverfloDetect : overflowdetector port map(
+Amost => A(3),
+Bmost => Bother(3),
+Summost => Sum(3),
+overflo => Overflow
+);
+
+bit0 : bitadder port map(
+A => A(0),
+B => B(0),
+Cin => SubChoice,
+Sum => Sum(0),
+Cout => Cin1
+);
+
+bit1 : bitadder port map(
+A => A(1),
+B => B(1),
+Cin => Cin1,
+Sum => Sum(1),
+Cout => Cin2
+);
+
+bit2 : bitadder port map(
+A => A(2),
+B => B(2),
+Cin => Cin2,
+Sum => Sum(2),
+Cout => Cin3
+);
+
+bit3 : bitadder port map(
+A => A(3),
+B => B(3),
+Cin => Cin3,
+Sum => Sum(3),
+Cout => Cout
+);
+
+end Structural;
---
+++ Lab2/Lab2_4bit_testbench.vhd
@@ -0,0 +1,126 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   22:50:28 02/13/2014
+-- Design Name:   
+-- Module Name:   C:/Users/C16Ryan.Redhead/Desktop/ECE 281/Lab2/lab2_4bit_testbench.vhd
+-- Project Name:  Lab2
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Lab2_4bit_final
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+
+USE ieee.std_logic_unsigned.all;
+
+ENTITY lab2_4bit_testbench IS
+END lab2_4bit_testbench;
+ 
+ARCHITECTURE behavior OF lab2_4bit_testbench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Lab2_4bit_final
+    PORT(
+         A : IN  std_logic_vector(3 downto 0);
+         B : IN  std_logic_vector(3 downto 0);
+         SubChoice : IN  std_logic;
+         Sum : INOUT  std_logic_vector(3 downto 0);
+         Cout : OUT  std_logic;
+         Overflow : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic_vector(3 downto 0) := (others => '0');
+   signal B : std_logic_vector(3 downto 0) := (others => '0');
+   signal SubChoice : std_logic := '0';
+
+ 	--Outputs
+   signal Sum : std_logic_vector(3 downto 0);
+   signal Cout : std_logic;
+   signal Overflow : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+  --constant <clock>_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Lab2_4bit_final PORT MAP (
+          A => A,
+          B => B,
+          SubChoice => SubChoice,
+          Sum => Sum,
+          Cout => Cout,
+          Overflow => Overflow
+        );
+
+   -- Clock process definitions
+  -- <clock>_process :process
+  -- begin
+	--	<clock> <= '0';
+	--	wait for <clock>_period/2;
+	--	<clock> <= '1';
+	--	wait for <clock>_period/2;
+  -- end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+     -- wait for <clock>_period*10;
+
+      -- insert stimulus here 
+
+		A <= "0000";
+		B <= "0000";
+		SubChoice <= '0';
+		for I in 0 to 15 loop
+			for J in 0 to 15 loop
+				wait for 10 ns;
+				B <= B + "0001";
+			end loop;
+			A <= A + "0001";
+		end loop;
+		
+		SubChoice <= '1';
+		for I in 0 to 15 loop
+			for J in 0 to 15 loop
+				wait for 10 ns;
+				B <= B + "0001";
+			end loop;
+			A <= A + "0001";
+		end loop;
+		 
+			 
+      wait;
+   end process;
+
+END;
